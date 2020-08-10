8200e75733bae8ace57b0037752a2c22ee6e6a4f
Mypaxa4e <ivanshtypula@gmail.com>
2020-08-10 15:17:24 +0300

    Updates

---
+++ precache-manifest.691de130caa49534d8a6ff6e6472529a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d72aa3571df57816f5993b5330dd9f6",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "ee1e416743b9e28fb777",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.317fefc6.chunk.css"
+  },
+  {
+    "revision": "bb9992c5ddb19b4de4be",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.e7ee531d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.e7ee531d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1e416743b9e28fb777",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.fcc8978f.chunk.js"
+  },
+  {
+    "revision": "7671957cb5e802151d00",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.2f76ee75.js"
+  }
+]);
